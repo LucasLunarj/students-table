a80648355359854efc712ca29613f8cb2fb22712
LucasLunarj <87677530+LucasLunarj@users.noreply.github.com>
2023-08-04 22:36:05 -0300

    deploy changes

--- src/components/StudentsGrade.jsx
+++ src/components/StudentsGrade.jsx
@@ -8,11 +8,11 @@ export const StudentsGrade = () => {
     return (
 
 
-        < div className='bg-slate-300 mx-14  '>
-            <table className='table-auto w-full md:table-fixed'>
+        < div className='bg-slate-300 md:mx-14  '>
+            <table className=' w-full table-fixed md:table-auto'>
                 <thead className='w-full h-auto border-solid border-1 border-black bg-slate-800 text-white'>
                     <tr className='text-left'>
-                        <th className=' text-left  py-3 pl-2' scope="col"><p>Nome</p> </th>
+                        <th className=' text-left text-sm  md:py-3 md:pl-2 w-30' scope="col"><p>Nome</p> </th>
                         <th className='pl-3' scope="col"><p>Status</p></th>
                         <th className='pl-3' scope="col"><p>Grade 1</p></th>
                         <th className='pl-3' scope="col"><p>Grade 2</p></th>
--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -5,7 +5,7 @@ export const UserInfo = (props) => {
         <>
             <tr className="">
                 <td>
-                    <div className="flex flex-col p-2 border-black md:flex-row">
+                    <div className="flex flex-col md:p-2 pb-10 border-black md:flex-row">
                         <img className="rounded-full w-16 h-16"
                             src={props.avatarImg}
                             alt="" />
@@ -13,22 +13,22 @@ export const UserInfo = (props) => {
                             <p className="font-bold">
                                 {props.name}
                             </p>
-                            <p>{props.email}</p>
+                            <p className="text-xs">{props.email}</p>
                         </div>
                     </div>
                 </td>
                 <td>
-                    <div>
-                        {whatIsTheStats === true ? <div>Active</div> : <div>Inactive</div>}
+                    <div className="">
+                        {whatIsTheStats === true ? <div className=" text-white border-solid bg-green-500 text-center rounded-lg  md:w-20 ">Active</div> : <div className="text-white border-solid bg-red-600 text-center rounded-lg md:w-20 ">Inactive</div>}
                     </div>
                 </td>
                 <td>
-                    <p>{props.grade1}</p>
+                    <p className="text-left pl-2 md:pl-3 ">{props.grade1}</p>
                 </td>
                 <td>
-                    <p>{props.grade2}</p>
+                    <p className="pl-2 md:text-left md:pl-3">{props.grade2}</p>
                 </td>
-                <td>{finalGradeTotal}</td>
+                <td className="md:pl-3">{finalGradeTotal}</td>
             </tr >
 
 
